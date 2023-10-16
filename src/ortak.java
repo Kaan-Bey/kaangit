eb20a8be478bcdb3bc6d3249e61c69ccbef35bee
Your Name <orakomer2551@gmail.com>
2023-10-17 00:08:21 +0300

    17/10/2023

--- src/ortak.java
+++ src/ortak.java
@@ -3,7 +3,7 @@ public class ortak {
 
     public static void main(String[] args) {
 
-        System.out.println("QA dünyasına hoş geldik mi");
+        System.out.println("ben böyle olsun istiyorum");
         System.out.println("Hoş bulduk.");
 
         System.out.println("Çalışmalarınız için teşekkür ederiz");
